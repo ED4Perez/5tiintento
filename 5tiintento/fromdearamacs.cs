d50e976a732c73acbbc1c42126a2ed1a0c4e530f
Tabita Sarai Arias Sanchez <a24214890@alumnos.uady.mx>
2026-05-14 11:25:36 -0600

    avances chamna

--- 5tiintento/fromdearamacs.cs
+++ 5tiintento/fromdearamacs.cs
@@ -16,5 +16,10 @@ namespace _5tiintento
         {
             InitializeComponent();
         }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
